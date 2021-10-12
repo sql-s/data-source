f4d179a566d25841ea5bc1952a1d22cedfb00f06
azathoth <vladislavutkin92@gmail.com>
2021-10-12 20:16:10 +0600

    fix the problem

--- src/main/java/ru/tcreator/jdbcstart/controller/Controller.java
+++ src/main/java/ru/tcreator/jdbcstart/controller/Controller.java
@@ -1,17 +1,13 @@
 package ru.tcreator.jdbcstart.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.tcreator.jdbcstart.repository.Repository;
 
-import javax.websocket.server.PathParam;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
